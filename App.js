d3706566cfd66d476229b3e81a3f850b23358a75
sunandasamanta <samantasunanda724@gmail.com>
2023-02-04 23:15:15 +0530

    into the express js

---
+++ App.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const app = express();
+const PORT = 3001 || process.env.PORT
+const twilio = require("twilio")
+
+const accountSid = "AC940a2df0c52e7b1fd038404b2df8dacb"
+const authToken = "2f1c0f9a9afd5b3a5c36ce946f12919e"
+
+const client = new twilio(accountSid, authToken);
+
+app.use(express.json());
+
+// Static files
+app.use(express.static("public"));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/img', express.static(__dirname + '/public/img'));
+
+// Set Views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+app.get('/send-text', (req, res) => {
+    //Welcome Message
+    res.send('Hello to the Twilio Server')
+
+    //_GET Variables
+    // const { textmessage, texternumber, carCompany, floorArea, location } = req.query;
+
+
+    //Send Text
+    client.messages.create({
+        body: "YOOO BOIII",
+        to: "+917866023319",  // Text this number
+        from: "+19786435240" // From a valid Twilio number
+    }).then((message) => console.log(message.body));
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`)
+})
